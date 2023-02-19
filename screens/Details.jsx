9a3079b787ad4c885fccd71869238edb01b7d7fa
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-02-19 13:46:50 +0100

    feat: created details page base template

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, FlatList } from 'react-native'
 import React from 'react'
+import FocusedStatusBar from '../components/focused-status-bar'
+import { COLORS } from '../constants'
+import CircleButton from '../components/circle-button'
+import NFTTitle, { ETHPrice, ReactBtn } from "../components/nft-title";
 
 const Details = () => {
   return (
+    <SafeAreaView style={{flex: 1, backgroundColor: "white"}}>
+      <FocusedStatusBar />
     <View>
       <Text>Details</Text>
     </View>
+    </SafeAreaView>
   )
 }
 
